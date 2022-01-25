e39cd10a515f1c6edfae40defaa753b6345bda32
mvdbeek <m.vandenbeek@gmail.com>
2022-01-25 20:50:02 +0100

    Build openpbs image instead of torque

---
+++ openpbs/docker_entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HOST=$(hostname)
+echo "\$clienthost $HOST" > /var/spool/pbs/mom_priv/config
+cat <<EOT > /etc/pbs.conf
+PBS_EXEC=/opt/pbs
+PBS_SERVER=$HOST
+PBS_START_SERVER=1
+PBS_START_SCHED=1
+PBS_START_COMM=1
+PBS_START_MOM=1
+PBS_HOME=/var/spool/pbs
+PBS_CORE_LIMIT=unlimited
+PBS_SCP=/bin/scp
+EOT
+
+/opt/pbs/libexec/pbs_init.d start
+
+# Run whatever the user wants to
+exec "$@"
